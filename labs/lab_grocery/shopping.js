b2bdd58ed274a1d7efd67d00e4bc04a9567483da
Luke Sathrum <sathrumlu@butte.edu>
2020-11-08 21:41:47 -0800

    Adding Lab Grocery

---
+++ labs/lab_grocery/shopping.js
@@ -0,0 +1,24 @@
+/*
+ * Filename:    shopping.js
+ * Description: FILL IN
+ * Author:      FILL IN
+ */
+
+
+// CREATE YOUR GLOBAL GROCERY LIST ARRAY
+const list = [];
+
+// Global Object Constructor / Class Syntax
+
+
+// Event Listener to Process Form Submission
+
+
+// Event Listener to Handle Removal of First Item
+
+
+// Event Listener to Handle Removal of Last Item
+
+
+// Function to Update the Text of the List
+
