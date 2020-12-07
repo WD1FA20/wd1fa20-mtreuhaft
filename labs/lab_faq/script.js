56327a6f03c062c0e69399ca1ad68de0f0939a99
Melanie Treuhaft <treuhaftme@butte.edu>
2020-12-07 19:31:55 +0000

    Adding FAQ Application

--- labs/lab_faq/script.js
+++ labs/lab_faq/script.js
@@ -1,27 +1,21 @@
 "use strict";
 
-//toggle paragraphs on and off  NOT WORKING
-$('.toggle').on('click', function() {
+//toggle paragraphs on and off  NOT WORKING -WHY?
+$('#toggle').on('click', function() {
   $(this).next().toggleClass('hide');
 });
 
+//add user's question and answer to the list
 $('#append').on('click', () => {
   const $addLi = $('<li>');
   const input = document.getElementById('question');
-  $addLi.text(input.value);
+  $addLi.html(input.value);  ///why can't i add a button here?
   $addLi.appendTo($('#FAQs'))
+  const $addP = $('<p>');
+  const input2 = document.getElementById('answer');
+  $addP.html(input2.value);
+  $addP.appendTo($('#FAQs'))
 });
-
-
-//add new entry   NOT WORKING
-//$('#submit').on('click', () => {
-  //const $appendli = $('<li>');
-  //$appendli.text('#question.value');
-  //$appendli.appendTo($('#FAQs'));
-  //const $appendp = $('<p>');
-  //$appendp.text('#answer.value');
-  //$appendp.appendTo($('#FAQs'));
-//});
 
 //extra credit buttons at the tomp
 $('#hide-all').on('click', () => {
