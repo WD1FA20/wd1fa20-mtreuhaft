f19c263ed545a01a3ce1b24cf4f3a2eda5340696
Melanie Treuhaft <treuhaftme@butte.edu>
2020-12-07 07:58:05 +0000

    Adding Lab Design

--- labs/lab_faq/script.js
+++ labs/lab_faq/script.js
@@ -5,15 +5,23 @@ $('.toggle').on('click', function() {
   $(this).next().toggleClass('hide');
 });
 
-//add new entry   NOT WORKING
-$('#submit').on('click', () => {
-  const $appendli = $('<li>');
-  $appendli.text('#question.value');
-  $appendli.appendTo($('#FAQs'));
-  const $appendp = $('<p>');
-  $appendp.text('#answer.value');
-  $appendp.appendTo($('#FAQs'));
+$('#append').on('click', () => {
+  const $addLi = $('<li>');
+  const input = document.getElementById('question');
+  $addLi.text(input.value);
+  $addLi.appendTo($('#FAQs'))
 });
+
+
+//add new entry   NOT WORKING
+//$('#submit').on('click', () => {
+  //const $appendli = $('<li>');
+  //$appendli.text('#question.value');
+  //$appendli.appendTo($('#FAQs'));
+  //const $appendp = $('<p>');
+  //$appendp.text('#answer.value');
+  //$appendp.appendTo($('#FAQs'));
+//});
 
 //extra credit buttons at the tomp
 $('#hide-all').on('click', () => {
